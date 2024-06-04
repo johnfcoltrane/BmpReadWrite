ee1c2ae5cfc7d9c707d4bd12ce0b354d83f558ef
johnfcoltrane <hy.p901is@gmail.com>
2024-06-04 22:55:54 +0900

    update

--- TestGetIndex.cpp
+++ TestGetIndex.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#define VOUT(x) cout << #x << "=" << x << endl;
+
 using namespace std;
 
 /*
@@ -46,7 +49,7 @@ for($row=0; $row -lt $height;$row++){
 **/
 int getIndex(int row, int col, int rgb, int realSize)
 {
-    return((col + (realSize*row)) * 3 + rgb);
+    return((col*3 + (realSize*row)) + rgb);
 }
 /**
  *  --> row, col, rgb
@@ -54,29 +57,38 @@ int getIndex(int row, int col, int rgb, int realSize)
  */
 void Bunkai(int idx, int realsize, int &row, int &col, int &rgb)
 {
+#if 0
     rgb = idx % 3;
     int pixaddr = (idx - rgb) / 3;
     col = pixaddr % realsize;
     row = (pixaddr - col) / realsize;
-    //($row,$col,$rgb)
+#endif
+    row = idx / realsize;
+	col = (idx % realsize) / 3;
+	rgb = (idx % realsize) % 3;
+	//($row,$col,$rgb)
 }
 /**
  * calc real linesize
  */
 int getRealsize(int w)
 {
-    return(w * 4 + w % 4);
+    return(w * 3 + w % 4);
 }
 int main(){
-
-int width = 30;
+vector<int> v;
+int width = 40;
 int height = 20;
+	int realsize = getRealsize (width);
 	for(int row=0; row < height;row++){
 		for(int col=0; col < width;col++){
-			int realsize = getRealsize (width);
 			//#"realsize={0}" -f $realsize
 			for(int rgb=0; rgb < 3; rgb++){
-				int ix = getIndex(row,  col, rgb,realsize) ;
+				int ix = getIndex(row,  col, rgb, realsize) ;
+				if (ix==360){
+					do{}while(0);
+				}
+				v.push_back(ix);
 				//#"ix=$ix"
 				//($row1,$col1,$rgb1) = (bunkai $ix $realsize)
 				int row1, col1, rgb1;
@@ -94,4 +106,5 @@ int height = 20;
 		}
 	}
 	cout << "OK" << endl;
+	VOUT(v.size());
 }
