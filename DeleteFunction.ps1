b908c80afed36bc7d6f9c58373d617191806aa6b
johnfcoltrane <hy.p901is@gmail.com>
2024-06-15 07:14:59 +0900

    tools

---
+++ DeleteFunction.ps1
@@ -0,0 +1,15 @@
+﻿#DeleteFunction.ps1
+Param($fnc)
+#try{
+#gi function:isSectionHDR
+#$fnc="isSectionHDR"
+if (test-Path function:$fnc){
+    rm function:$fnc
+    "削除しました。[function;$fnc]"
+}
+else{
+    "[function:$fnc]は存在しません。"
+}
+#catch{
+#$_
+#}
---
+++ analyze_Ipconfig.ps1
@@ -0,0 +1,44 @@
+﻿chcp 437
+$a=ipconfig
+function isSectionHDR($_){
+$_[0] -ne ' ' -and $_ -and $_[-1] -eq ':'
+}
+$h=@{}
+$buf=@()
+$aa=@()
+$hdr_bak=""
+$sections=@()
+($a+"END:")|%{ 
+    if(isSectionHDR $_){
+        #"-------- $_ ----------" 
+        $aa += @(,$buf)
+        $sections+=$_
+        $buf=@()
+    } 
+    else{
+        if ($_) {
+            $buf += $_
+        }
+    }
+}
+#$sections
+$aa = $aa[1..($aa.Count-1)]
+function chk($a, $s){
+    do{
+    $a|%{
+        if ($_ -match $s){
+            $false
+            break
+        }
+    }
+    $true
+    }until(1)
+}
+0..($aa.Count-1)|%{
+    if((chk $aa[$_] ": Media disconnected")){
+        "------------- $($sections[$_]) -------------"
+        $aa[$_]
+    }
+}|ogv -Title "analyze ipconfig"
+$aa.Count
+$sections.count
