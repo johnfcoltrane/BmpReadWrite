d6278ba3b7496fee3477feef72ddc5e7cf67fe40
johnfcoltrane <hy.p901is@gmail.com>
2024-06-30 11:17:01 +0900

    Add files via upload

---
+++ GG.ps1
@@ -0,0 +1,14 @@
+﻿$qs=""
+$a=$ARGS|%{
+    if ($_ -match "#"){
+        $_ -replace "#","%23"
+    }
+    else{
+        $_
+    }
+}|%{ 
+    if ($qs) { $qs += "+" }
+    $qs += $_
+}
+$qs
+start "https://www.google.co.jp/search?q=$qs"
---
+++ GetOriginalPath.ps1
@@ -0,0 +1,5 @@
+﻿# get original path from subst-drive (GetOriginalPath)
+Param($d="t", [switch]$i=$false)
+if ($i) {"Drive=$d"}
+$p = ((cmd /c "subst|find /i ""$d`:""") -split " ")[2]
+$p
